d1b1824ec7bf7bdb050aee62cfeb9b20fc54337d
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-02-12 07:41:56 +0600

    validate login form using Joi lib

--- src/component/reusing/loginForm.jsx
+++ src/component/reusing/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 
 import Input from './input';
 
@@ -9,23 +10,13 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string().min(3).max(30).required().label('Username'),
+    password: Joi.string().required().label('Password')
+  }
+
   // username = React.createRef(); // work like document.object.getElementById()
   // e = event object
-
-  validate = () => {
-
-    const errors = {};
-
-    const { account } = this.state;
-
-    if (account.username.trim() === '' ) 
-      errors.username = 'Username is required.';
-      
-    if (account.password.trim() === '' ) 
-      errors.password = 'Password is required.';
-
-    return Object.keys(errors).length === 0 ? null : errors ;  
-  };
 
 
   handleSubmit = e =>{
@@ -38,16 +29,55 @@ class LoginForm extends Component {
     
   };
 
-  validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-      
-    }
+  validate = () => {
+    const option = { abortEarly: false};
+    const { error } = Joi.validate(this.state.account, this.schema, option);
+    
+    if(!error) return null;
 
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-    }
-  }
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+
+  // const result = Joi.validate(this.state.account, this.schema, { abortEarly: false});
+  // if(!result.error) return null;
+    // const errors = {};
+    // for (let item of result.error.details) errors[item.path[0]] = item.message;
+
+    return errors;
+
+    // const errors = {};
+
+    // const { account } = this.state;
+
+    // if (account.username.trim() === '' ) 
+    //   errors.username = 'Username is required.';
+      
+    // if (account.password.trim() === '' ) 
+    //   errors.password = 'Password is required.';
+
+    // return Object.keys(errors).length === 0 ? null : errors ;  
+
+  };
+
+
+  validateProperty = ({ name, value }) => {
+
+    const obj = { [name]: value};
+    const schema = { [name]: this.schema[name] };
+
+    const {error} = Joi.validate(obj, schema);
+
+    return error ? error.details[0].message : null;
+
+    // if (name === "username") {
+    //   if (value.trim() === "") return "Username is required.";
+      
+    // }
+
+    // if (name === "password") {
+    //   if (value.trim() === "") return "Password is required.";
+    // }
+  };
 
   handleChange = ({ currentTarget: input }) => {
 
@@ -87,8 +117,8 @@ class LoginForm extends Component {
               label = 'Password'
               error = {errors.password}
             />
-      
-          <button className="btn btn-primary">Submit</button>
+
+          <button disabled={this.validate()} className="btn btn-primary">Login</button>
         </form>
       </div>
      );
