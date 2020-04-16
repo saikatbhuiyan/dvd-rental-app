6fd60bf1a3c87e2cd14e195b19c6e4b0f79d43d0
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-17 01:50:37 +0600

    post put

--- src/component/movieForm.jsx
+++ src/component/movieForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Joi from 'joi-browser';
 
 import Form from "./reusing/form";
-import { getGenres } from '../services/fakeGenreService';
+import { getGenres } from '../services/genreService';
 import { saveMovie, getMovie } from '../services/fakeMovieService';
 
 
@@ -44,17 +44,31 @@ class MovieForm extends Form {
     
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async populateGenres() {
+    const {data: genres} = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === 'new') return;
-    const movie = getMovie(movieId);
+  async populateMovie() {
+    try {
+      const movieId = this.props.match.params.id;
+      if (movieId === 'new') return;
+  
+      const { data: movie} = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+      
+    } catch (ex) {
+      if (ex.respose && ex.respose.status === 404){
+        this.props.history.replace("/not-found");
+      }
+    }
+  }
 
-    if (!movie) return this.props.history.replace("/not-found");
+  async componentDidMount() {
+   await this.populateGenres();
+   await this.populateMovie();
+  
 
-    this.setState({ data: this.mapToViewModel(movie) });
   }
 
   mapToViewModel(movie) {
@@ -67,8 +81,8 @@ class MovieForm extends Form {
     };
   }
 
-  doSubmit = () => {
-    saveMovie(this.state.data);
+  doSubmit =async () => {
+    await saveMovie(this.state.data);
 
     this.props.history.push("/movies");
   };
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -8,6 +8,21 @@ export function getMovies() {
   return http.get(apiEndpoint);
 }
 
+
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + movieId);
+}
+
+export function saveMovie(movie) {
+  if (movie.id) {
+    const body = {...movie};
+    delete body.id;
+    return http.put(apiEndpoint + movie.id, body);
+  }
+  return http.post(apiEndpoint, movie);
+}
+
+
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + movieId);
 }
